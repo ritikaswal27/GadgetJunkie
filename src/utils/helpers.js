ae84808327099877b1f661fb5b89bde1878f5794
ritikaswal27 <ritikaswal2705@gmail.com>
2023-04-19 22:16:06 +0530

    implement single product page

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,9 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  const newNumber = Intl.NumberFormat('en-In', {
+    style: 'currency',
+    currency: 'INR',
+  }).format(number / 100);
+  return newNumber;
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
